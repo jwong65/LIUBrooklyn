70bae268e88a263838cb5667116cb090da085461
Jason Wong <jason.wong2@my.liu.edu>
2018-03-05 15:51:25 -0500

    Fixed the buttons and the on click functions

--- assn3-map/assignment.js
+++ assn3-map/assignment.js
@@ -1,5 +1,7 @@
 var map =null;
 var warsaw = {lat: 52.2581, lng: 21.0216};
+var sydney = {lat: -33.8688, lng: 151.2093};
+var singapore = {lat: 1.3521, lng: 103.8198};
 
 
 function initMap()
@@ -17,20 +19,48 @@ function initMap()
 			map: map,
 			title: 'Warsaw Zoo'
 	});
+	var marker2 = new google.maps.Marker({
+			position: sydney,
+			map: map,
+			title: 'Sydney'
+	});
+	var marker3 = new google.maps.Marker({
+			position: singapore,
+			map: map,
+			title: 'Singapore'
+	});
 	
 	marker1.addListener('click', () => clickCity(0));
+	marker2.addListener('click', () => clickCity(1));
+	marker3.addListener('click', () => clickCity(2));
 }
 	
 
 	function clickCity(cityId)
 	{
-		var pic = document.getElementById("pic")
-		console.log("A city has been clicked", cityId)
+		var pic = document.getElementById("swapthepicture")
+		console.log("A location has been clicked, location #", cityId)
 		map.setZoom(15);
 		switch(cityId){
 		case 0:
 			map.setCenter(warsaw);
+			swapthepicture.src = "warsaw-zoo.jpg"
+			paragraph.innerHTML = "This is what happens."
+			document.getElementById("paragraph").style.color = "green";
+			paragraph.style.backgroundColor = "#f789"
 			break;
-	}
+		case 1:
+			map.setCenter(sydney);
+			swapthepicture.src = "sydney.jpg"
+			paragraph.innerHTML = "Sydney in Australia would be an interesting place to visit, but I have never visited Australia."
+			document.getElementById("paragraph").style.color= "red";
+			paragraph.style.backgroundColor = "#124"
+			break;
+		case 2:
+			map.setCenter(singapore);
+			swapthepicture.src = "singapore.jpg"
+			break;
+		}
+		
 	}
 	
