95f778408548bb6d8bc5c6a870349ce3bfe5e355
Jason Wong <jason.wong2@my.liu.edu>
2018-03-05 09:59:30 -0500

    Fixed the .js file

--- assn3-map/assignment.js
+++ assn3-map/assignment.js
@@ -1,13 +1,15 @@
 var map =null;
 var warsaw = {lat: 52.2581, lng: 21.0216};
 
-console.log("Is this working?")
 
 function initMap()
 {
 	console.log("Begin")
+	var div = document.getElementById("thismap")
+	console.log(div)
 	
 	map= new google.maps.Map(div, {
 		zoom: 15, 
 		center: warsaw
 	});
+}
