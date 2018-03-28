f97b7c4bb89b3ae4aa7980ef8ddaa6c1e65a3623
Jason Wong <jason.wong2@my.liu.edu>
2018-03-28 15:47:15 -0400

    Fixed the Stringify section in the .js

--- assn4-mesg/ajax.js
+++ assn4-mesg/ajax.js
@@ -1,1 +1,47 @@
 console.log("This is ajax")
+
+var moods=[]
+
+function moodList(){
+	$.ajax("http://cs120.liucs.net/assn4/moods.json",
+		{
+			success: function(data) {
+				moods = data
+				$("datalist#mood").empty()
+				moods.map(function(m){
+					$("datalist#mood").append("<option value='"+m+"'>")
+				})
+
+		},
+		})
+	setTimeout(moodList, 10000)
+}
+
+$(function() {
+	moodList()
+	
+	console.log("Mood Listing")
+	$("input#press").click(function(){
+		console.log("Button has been clicked")
+		$.ajax("http://cs120.liucs.net/assn4/messages.json",
+			{
+				method: "POST",
+				data: JSON.stringify(
+				{		sender: $("input#sender").val(),
+				mood: $("input#moody").val(),
+				text: $("input#message").val()}),
+                   error: function() {},
+                   success: function() {
+                       $("input#text").val("")
+
+					},
+			})
+	})
+})
+
+
+$(document).ready(function () {
+    $.getJSON("http://cs120.liucs.net/assn4/messages.json", function (response) {
+        console.log(response);
+    });
+});
