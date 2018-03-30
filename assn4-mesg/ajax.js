bf6345ba9923d74f0f2a461a83006ce6876284d8
Jason Wong <jason.wong2@my.liu.edu>
2018-03-30 12:30:33 -0400

    Finallly got a table working

--- assn4-mesg/ajax.js
+++ assn4-mesg/ajax.js
@@ -43,5 +43,47 @@ $(function() {
 $(document).ready(function () {
     $.getJSON("http://cs120.liucs.net/assn4/messages.json", function (response) {
         console.log(response);
-    });
+			var arrayResponses =[];
+		$.each(response, function (index, value){
+			arrayResponses.push(value);
+		});
+	
+	var col = [];
+	for (var i=0; i<arrayResponses.length; i++)
+	{ 
+		for (var key in arrayResponses[i]) 
+			{
+				if (col.indexOf(key) === -1) {
+				col.push(key);
+				}
+			}
+	}
+	
+	
+	var table = document.createElement("table");
+	
+	var tr = table.insertRow(-1);
+	
+	for (var i=0; i<col.length; i++)
+	{ 
+	var th = document.createElement("th");
+	th.innerHTML = col[i];
+	tr.appendChild(th);
+	}
+	
+	for (var i=0; i<arrayResponses.length; i++)
+	{
+		tr = table.insertRow(-1);
+		
+		for (var j=0; j<col.length; j++)
+		{
+		var tabCell = tr.insertCell(-1);
+		tabCell.innerHTML = arrayResponses[i] [col[j]];
+		}
+	}
+
+	var divContainer = document.getElementById("responselist");
+	divContainer.innerHTML = "";
+	divContainer.appendChild(table);
+});
 });
